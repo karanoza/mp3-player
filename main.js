cbd7589214b4d16d5a985ee806910b69f6fa5938
Karan <karanoza94@gmail.com>
2020-08-18 00:16:10 +0530

    Mp3 player created

---
+++ main.js
@@ -0,0 +1,44 @@
+music_name = "./images/Music.mp3";
+let play_btn = document.querySelector("#play");
+let prev_btn = document.querySelector("#pre");
+let next_btn = document.querySelector("#next");
+let range = document.querySelector("#range");
+let play_img = document.querySelector("#play_img");
+let total_time = 0;
+let currentTime = 0;
+let duration = 0;
+let isPlaying = false;
+let song = new Audio();
+window.onload = playSong;
+
+function playSong() {
+  song.src = music_name;
+  console.log(song);
+
+  play_btn.addEventListener("click", function () {
+    if (!isPlaying) {
+      song.play();
+      isPlaying = true;
+      total_time = song.duration;
+      range.max = total_time;
+      play_img.src = "./images/pause.png";
+    } else {
+      song.pause();
+      isPlaying = false;
+      play_img.src = "./images/play.pn";
+    }
+    song.addEventListener("ended", function () {
+      song.currentTime = 0;
+      song.pause();
+      isPlaying = false;
+      range.value = 0;
+      play_img.src = "./images/play.png";
+    });
+    song.addEventListener("timeupdate", function () {
+      range.value = song.currentTime;
+    });
+    range.addEventListener("change", function () {
+      song.currentTime = range.value;
+    });
+  });
+}
